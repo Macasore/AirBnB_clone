08e0ed5d609bca906dd93c5055b1b8f3f48171a2
Macasore <macasorekingdavid@gmail.com>
2023-05-13 14:40:41 +0100

    feat:add the console.py file

---
+++ console.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""console
+
+This file contains the entry point of the command interpreter
+"""
+import cmd
+from models.base_model import BaseModel
+from models import storage
+
+class HBNBCommand(cmd.Cmd):
+    """
+    This class creates the command interpreter
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """quits command to exit the console
+        """
+        return True
+
+    def do_EOF(self, line):
+        """quits the console
+        """
+        return True
+
+    def emptyline(self):
+        pass
+
+    def do_create(self, line):
+        """creates a new instance and saves it
+        """
+        if (not line):
+            print("** class name missing **")
+        else:
+            if (line != "BaseModel"):
+                print("** class doesn't exist **")
+            else:
+                new_instance = BaseModel()
+                new_instance.save()
+                print(new_instance.id)
+
+    def do_show(self, line):
+        """prints the string representation of an instance based
+        on the class name and id
+        """
+        l = line.split()
+        if (not line):
+            print("** class name missing **")
+        else:
+            if (l[0] != "BaseModel"):
+                print("** class doesn't exist **")
+            else:
+                if ((len(l) == 2)):
+                    storage_dict = storage.all()
+                    for key in storage_dict:
+                        if key.endswith(l[1]):
+                            string = "yayy it works"
+                            break
+                        else:
+                            string = "** no instance found **"
+                    print(string)
+                else:
+                    print("** instance id missing **")
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""file_storage file
+this file contains the FileStorage class for
+serialization and deserialization
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """FileStorage class
+    this class contains the attribute and methods
+    required for serializing and deserializing objects
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """
+        instantiation method
+        """
+
+    def all(self):
+        """
+        returns a dictionary of available objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        adds the object to the existing file
+        """
+        FileStorage.__objects["{}.{}"
+                              .format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """
+        serializes the object file
+        """
+        json_obj = {}
+        for key, value in FileStorage.__objects.items():
+            json_obj[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as fp:
+            json.dump(json_obj, fp, indent=2)
+
+    def reload(self):
+        """
+        deserializes the json object
+        """
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                json_object = json.load(f)
+
+                for key, value in json_object.items():
+                    FileStorage.__objects[key] = BaseModel(**value)
+
+        except Exception:
+            pass
