7ce40c00ce9a6faf2b478da6f88f545a6317e8d6
Macasore <macasorekingdavid@gmail.com>
2023-05-15 21:45:58 +0100

    feat: update console.py

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ This file contains the entry point of the command interpreter
 """
 import cmd
 from models.base_model import BaseModel
-from models import storage
+from models import storage, avail_classes
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,14 +15,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """quit
-        quits command to exit the console
+        """quits command to exit the console
         """
         return True
 
     def do_EOF(self, line):
-        """EOF
-        quits the console
+        """quits the console
         """
         print()
         return True
@@ -45,28 +43,93 @@ class HBNBCommand(cmd.Cmd):
                 new_instance.save()
                 print(new_instance.id)
 
-    # def do_show(self, line):
-    #     """prints the string representation of an instance based
-    #     on the class name and id
-    #     """
-    #     l = line.split()
-    #     if (not line):
-    #         print("** class name missing **")
-    #     else:
-    #         if (l[0] != "BaseModel"):
-    #             print("** class doesn't exist **")
-    #         else:
-    #             if ((len(l) == 2)):
-    #                 storage_dict = storage.all()
-    #                 for key in storage_dict:
-    #                     if key.endswith(l[1]):
-    #                         string = "yayy it works"
-    #                         break
-    #                     else:
-    #                         string = "** no instance found **"
-    #                 print(string)
-    #             else:
-    #                 print("** instance id missing **")
+    def do_show(self, line):
+        """prints the string representation of an instance based
+        on the class name and id
+        """
+        value = find(line)
+        if value is None:
+            return
+        else:
+            print(value)
+
+    def do_destroy(self, line):
+        """deletes an instance based on the class's name and id
+        """
+        value = find(line)
+        if value is None:
+            return
+        else:
+            key = "{}.{}".format(value.__class__.__name__, value.id)
+            store = storage.all()
+            del store[key]
+            storage.save()
+
+    def do_all(self, line):
+        """prints a string representation of all instance based
+        on the class or not
+        """
+        list = storage.all()
+        str_list = []
+        val = line.split()
+        if (not line):
+            for values in list.values():
+                str_list.append(str(values))
+            print(str_list)
+            return
+        if (line not in avail_classes):
+            print("** class doesn't exist **")
+        else:
+            for values in list.values():
+                str_list.append(str(values))
+            print(str_list)
+
+    def do_update(self, line):
+        """updates an instance
+        """
+        value = find(line)
+        if value is None:
+            return
+        else:
+            split = line.split()
+            # if (split[2] not in value.keys()):
+            #     print("** attribute name missing **")
+            if len(split) >= 4:
+                setattr(value, split[2], split[3])
+                value.save()
+            elif len(split) == 2:
+                print("** attribute name missing **")
+            elif len(split) == 3:
+                print("** value missing **")
+
+
+def find(line):
+    """checks for an object in a storage
+    """
+    attr = line.split()
+    if (not line):
+        print("** class name missing **")
+        return None
+    else:
+        if (attr[0] != "BaseModel"):
+            print("** class doesn't exist **")
+            return None
+        else:
+            if ((len(attr) == 1)):
+                print("** instance id missing **")
+            else:
+                fin = get_obj(attr[0], attr[1])
+                if fin is None:
+                    print("** no instance found **")
+                return fin
+
+
+def get_obj(cls_name, id):
+    """gets the object of the class given
+    """
+    key = "{}.{}".format(cls_name, id)
+    storage_dict = storage.all()
+    return (storage_dict.get(key))
 
 
 if __name__ == '__main__':
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 from .engine.file_storage import FileStorage
+from .base_model import BaseModel
 storage = FileStorage()
 storage.reload()
+
+avail_classes = {
+	'BaseModel': BaseModel
+}
