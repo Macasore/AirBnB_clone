0681f79ecc4223eb9e435cd03b692c8411f7fd83
Macasore <macasorekingdavid@gmail.com>
2023-05-17 02:41:51 +0100

    add init file

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,21 @@
 from .engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
+from .amenity import Amenity
+from .city import City
+from .place import Place
+from .review import Review
+from .state import State
 
 
 avail_classes = {
     'BaseModel': BaseModel,
-    'User': User
+    'User': User,
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'Review': Review,
+    'State': State,
 }
 storage = FileStorage()
 storage.reload()
